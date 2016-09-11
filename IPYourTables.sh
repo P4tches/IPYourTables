0fe2575dc1e1a9ec1a11c88710d4a7714024062a
jonmax10 <jonmax10@debian>
2016-09-11 07:24:29 -0700

    Updated for port ranges

--- IPYourTables.sh
+++ IPYourTables.sh
@@ -5,15 +5,36 @@ ut=$(echo $input | cut -d ':' -f 1)
 port=$(echo $input | cut -d ':' -f 2)
 io=$(echo $input | cut -d ':' -f 3)
 #echo "yes"
+#if[ $port>='1' || port<='65565']
 protocol=$ut
 if [ $ut = "t" ]; then
 	protocol="tcp"
-else
+elif [ $ut = "u" ]; then
 	protocol="udp"
-fi
-if [ $io = "i" ]; then
-	echo $protocol":"$port":in"
 else
-	echo $protocol":"$port":out"
+echo "Try Again."
+fi
+s=' '
+port1=$(echo $port | cut -d '-' -f 1)
+port2=$(echo $port | cut -d '-' -f 2)
+echo "port1:"$port1
+if [ $io = "i" ]; then
+	if [[ "$port" == *"-"* ]]; then
+		for((i=$port1; i<=$port2; i++)); do
+			echo $protocol":"$i":in"
+		done
+	else
+		echo $protocol":"$port":in"
+	fi
+elif [ $io = "o" ]; then
+	if [[ "$port" == *"-"* ]]; then
+		for((i=$port1; i<=$port2; i++)); do
+			echo $protocol":"$i":out"
+		done
+	else
+		echo $protocol":"$port":out"
+	fi
+else
+echo "You fucked up"
 fi
 done
